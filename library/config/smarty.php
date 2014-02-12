a6f686cff197144b76bb4dc0d43deddf2f26fae6
tangxunye <tangxunye@91waijiao.com>
2014-02-12 13:51:06 +0800

    update lib config

---
+++ library/config/smarty.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['template_dir'] = APPPATH.'views';
+$config['compile_dir'] = APPPATH.'templates_c';
+$config['cache_dir'] = APPPATH.'cache';
+$config['caching'] = false;
+$config['debugging'] = false;
+$config['compile_check'] = false;
+$config['force_compile'] = false;
+$config['left_delimiter'] = '{';
+$config['right_delimiter'] = '}';
+
+/* End of file smarty.php */
+/* Location: ./library/config/smarty.php */     
