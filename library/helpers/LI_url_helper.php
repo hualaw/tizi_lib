737a291a4cafa6a444e296e7a0af770df05e5613
tangxunye <tangxunye@91waijiao.com>
2014-02-13 10:04:05 +0800

    update tizi url

--- library/helpers/LI_url_helper.php
+++ library/helpers/LI_url_helper.php
@@ -9,5 +9,22 @@ if ( ! function_exists('login_url'))
 	}
 }
 
+if ( ! function_exists('tizi_url'))
+{
+	function tizi_url($uri = '')
+	{
+		$CI =& get_instance();
+		return $CI->config->site_url($uri,'tizi');
+	}
+}
+
+if ( ! function_exists('jxt_url'))
+{
+	function jxt_url($uri = '')
+	{
+		$CI =& get_instance();
+		return $CI->config->site_url($uri,'jxt');
+	}
+}
 /* End of file LI_url_helper.php */
 /* Location: ./system/helpers/LI_url_helper.php */
