e327065a4d5ec5061b106f607bc3ecf133a26776
tangxunye <tangxunye@91waijiao.com>
2014-02-14 14:56:55 +0800

    update

--- library/config/captcha.php
+++ library/config/captcha.php
@@ -2,5 +2,9 @@
 $config['img_height'] = '34';
 $config['img_width'] = '113';
 $config['expiration'] = 60;
+
+$config['font_random'] = true;
 $config['font_path'] = LIBPATH.'views/static/fonts/';
 $config['font'] = array('default.ttf','ITCKRIST.TTF');
+
+$config['line_curve'] = false;
--- library/libraries/Captcha.php
+++ library/libraries/Captcha.php
@@ -6,24 +6,18 @@ class Captcha {
 
     private $_ci;          // CI object
     private $word;         // 验证码显示内容;
-    private $font_path;    // 字体文件路径;
-    private $font;          // 字体文件;
-    private $expiration;   // Session 过期时间;
-    private $img_height;   // 验证码高；
-    private $img_width;    // 验证码宽;
+    private $config;
 
     public function __construct(){
         $this->_ci = & get_instance();
         $this->_ci->load->library('session');
-        $this->_ci->load->config('captcha');
+        $this->_ci->load->config('captcha',true);
 		$this->_ci->load->helper('url');
         $this->_ci->load->helper('string');
         $this->_ci->load->helper('captcha');
         
         $this->setWord();
-        $this->setFontPath();
         $this->setImgOptions();
-        $this->setExpiration();
     }
 
     public function validateCaptcha($input_captcha_word,$page_name,$unset_word=true)
@@ -56,7 +50,8 @@ class Captcha {
         $captcha_token[$page_name]=$this->word;
         $this->_ci->session->set_userdata('captcha_token', json_encode($captcha_token));
 
-        $captcha_arr  = create_captcha($this->getOptions());
+        $this->config['word'] = $this->word;
+        $captcha_arr  = create_captcha($this->config);
         unset($captcha_arr['word']);
 
         return $captcha_arr;
@@ -66,28 +61,8 @@ class Captcha {
         $this->word = random_string('alnum','4');
     }
 
-    public function setFontPath(){
-        $this->font_path = $this->_ci->config->item('font_path');
-        $this->font = $this->_ci->config->item('font');
-    }
-
     public function setImgOptions(){
-        $this->img_width  = $this->_ci->config->item('img_width');
-        $this->img_height = $this->_ci->config->item('img_height');
+        $this->config = $this->_ci->config->item('captcha');
     }
 
-    public function setExpiration(){
-        $this->expiration = $this->_ci->config->item('expiration');
-    }
-
-    public function getOptions(){
-        return array(
-            'word'=>$this->word, 
-            'font_path'=>$this->font_path,
-            'font'=>$this->font,
-            'img_width'=>$this->img_width, 
-            'img_height'=>$this->img_height,
-            'expiration'=> $this->expiration
-        );
-    }
 }
