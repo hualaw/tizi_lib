723e244a1b2e84c384d62583a964ee2c4f95d562
liuhua <liuhua@91waijiao.com>
2014-02-22 14:54:52 +0800

    调整验证码字符字体和其他格式

--- library/config/captcha.php
+++ library/config/captcha.php
@@ -3,8 +3,9 @@ $config['img_height'] = '34';
 $config['img_width'] = '113';
 $config['expiration'] = 60;
 
-$config['font_random'] = false;
+$config['font_random'] = true;
 $config['font_path'] = LIBPATH.'views/static/fonts/';
-$config['font'] = array('default.ttf','the_score_normal.ttf');
+$config['font'] = array('default.ttf','the_score_normal.ttf', 'ITCKRIST.TTF', 'FORTE.TTF', 'BAUHS93.TTF', 'sf_hollywood_hills.ttf', 'detourDork.ttf', 'PostCrypt.ttf');
 
-$config['line_curve'] = false;
+$config['line_curve'] = true;
+$config['word_angle'] = 30;
