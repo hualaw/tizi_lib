77f538802f2d50bd9bb501cd1769e526565ba5b5
caohaihong <caohaihong@91waijiao.com>
2014-02-24 17:26:03 +0800

    家校通功能

--- library/models/jxt/ab_jxt_model.php
+++ library/models/jxt/ab_jxt_model.php
@@ -17,22 +17,7 @@ class Ab_Jxt_Model extends JXT_Model{
      */
     public function set_ab_id($phone,$user_id){
         $this->database->where("phone = '{$phone}' AND del = 0");
-        $this->database->update('address_book', array('user_id' => $user_id));
-    }
-    
-    /**
-     * 获取当前用户是否存在通讯录中
-     * 
-     * @param int $user_id
-     * @param string $field
-     * @return mixed
-     * 
-     * @author caohaihong <caohaihong@91waijiao.com>
-     */
-    public function get_ab($user_id, $field = '*'){
-        return $this->database->from('address_book')
-                ->where("user_id = {$user_id} AND del = 0")
-                ->get()->row_array();
+        return $this->database->update('address_book', array('user_id' => $user_id, 'phone' => ''));
     }
     
 }
