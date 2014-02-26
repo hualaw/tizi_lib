0a213578799184b6db0e8ce786aef2279933e4df
tangxunye <tangxunye@91waijiao.com>
2014-02-26 10:50:59 +0800

    update captcha

--- library/config/captcha.php
+++ library/config/captcha.php
@@ -3,10 +3,11 @@ $config['img_height'] = '34';
 $config['img_width'] = '113';
 $config['expiration'] = 60;
 
-$config['font_random'] = true;
+$config['font_random'] = false;
 $config['font_path'] = LIBPATH.'views/static/fonts/';
 //$config['font'] = array('default.ttf','the_score_normal.ttf', 'ITCKRIST.TTF', 'FORTE.TTF', 'BAUHS93.TTF', 'sf_hollywood_hills.ttf', 'detourDork.ttf', 'PostCrypt.ttf');
 $config['font'] = array('default.ttf','the_score_normal.ttf');
 
-$config['line_curve'] = true;
-$config['word_angle'] = 20;
+$config['line_curve'] = false;
+$config['word_angle'] = 15;
+$config['word_space'] = -4;
