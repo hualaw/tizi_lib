cc5388edd7e004dcccb64c5b6e7bd4252134fb62
tangxunye <tangxunye@91waijiao.com>
2014-03-24 09:51:12 +0800

    update

--- library/config/routes.php
+++ library/config/routes.php
@@ -1,8 +1,9 @@
 <?php
 /*zujuan login*/
 $route['logout']="login/login/logout";
+$route['logout/(:any)']="login/login/logout/$1";
 $route['login/submit']="login/login/submit";
 
 $route['check_code']="login/login/check_code";
-$route['check_captcha']="login/cap/validate";
-$route['captcha']="login/cap/generate";
+$route['check_captcha']="login/captcha_code/validate";
+$route['captcha']="login/captcha_code/generate";
