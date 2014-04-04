5c79c3d9ae7b372415a5ec97ff5a8ad15b359b60
tangxunye <tangxunye@91waijiao.com>
2014-04-04 16:56:22 +0800

    update

--- library/core/LI_Controller.php
+++ library/core/LI_Controller.php
@@ -305,7 +305,7 @@ class LI_Controller extends CI_Controller{
 			    	if($this->tizi_ajax)
 					{
 						$redirect=$this->input->get_post('redirect',true,false,'reload');
-						$reg_redirect=$this->input->get_post('reg_redirect',true);
+						$reg_redirect=$this->input->get_post('reg_redirect',true,true,'none');
 						$this->smarty->assign('login_url',login_url());
 						$this->smarty->assign('redirect',$redirect);
 						$this->smarty->assign('reg_redirect',$reg_redirect);
