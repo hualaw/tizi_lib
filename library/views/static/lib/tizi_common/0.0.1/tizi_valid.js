02065786c847700f0b3c1687c4877d1a960a1a73
tangxunye <tangxunye@91waijiao.com>
2014-04-04 17:42:04 +0800

    update

--- library/views/static/lib/tizi_common/0.0.1/tizi_valid.js
+++ library/views/static/lib/tizi_common/0.0.1/tizi_valid.js
@@ -9,7 +9,9 @@ define(function(require, exports) {
 
     exports.indexLogin = function(callback_login){
          // 加载placeHolder插件
-        require('placeHolder').JPlaceHolder.init();
+        seajs.use('placeHolder',function(ex){
+            ex.JPlaceHolder.init();
+        });
         var _Form = $(".indexLoginForm").Validform({
             // 自定义tips在输入框上面显示
             tiptype: function(msg, o, cssctl) {
