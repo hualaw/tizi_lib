f5d634031439b56908f42058511d34107ffce641
tangxunye <tangxunye@91waijiao.com>
2014-04-04 10:21:35 +0800

    update

--- library/libraries/LI_Session.php
+++ library/libraries/LI_Session.php
@@ -188,11 +188,7 @@ class LI_Session extends CI_Session {
 			if($this->_redis)
 			{
 				$userdata = $this->_redis_get($session_id);
-				if(!empty($userdata)) 
-				{
-					$userdata = json_decode($userdata,true);
-					$session = $userdata;
-				}
+				if(!empty($userdata)) $session = json_decode($userdata,true);
 			}
 			
 			if(empty($session))
