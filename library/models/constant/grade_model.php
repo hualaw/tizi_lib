e883813a68c92df880612d8152ad88c8cfdd8cdb
tangxunye <tangxunye@91waijiao.com>
2014-04-08 14:22:33 +0800

    update

--- library/models/constant/grade_model.php
+++ library/models/constant/grade_model.php
@@ -9,11 +9,26 @@ class Grade_model extends LI_Model{
         parent::__construct();
     }
 	
-    function get_grade($grade_type = 0, $return_array=false)
+    function get_grade($grade_type = 0, $return_array = true)
     {
     	if($grade_type) $this->db->where('grade_type',$grade_type);
+        $this->db->where('online',1);
+        $this->db->order_by('order,id','asc');
     	$query = $this->db->get($this->_table);
     	$result = $query->result();
+
+        if($return_array)
+        {
+            $grade = array();
+            foreach($result as $r)
+            {
+                if(!isset($grade[$r->grade_type][0])) $grade[$r->grade_type][0]='';
+                if($r->grade_type_name) $grade[$r->grade_type][0]=$r->grade_type_name;
+                $grade[$r->grade_type][$r->id]=$r->name;
+            }
+            $result = $grade;
+        }
+
     	return $result;
     }
 
