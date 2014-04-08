68930f50edf63c4a33a0db7d662dacc4a773bafc
tangxunye <tangxunye@91waijiao.com>
2014-04-08 12:07:10 +0800

    add grade model

---
+++ library/models/constant/grade_model.php
@@ -0,0 +1,17 @@
+<?php
+
+class Grade_model extends LI_Model{
+	
+	private $_table = "grade";
+
+	function __construct()
+    {
+        parent::__construct();
+    }
+	
+    function get_grade()
+    {
+
+    }
+
+}
