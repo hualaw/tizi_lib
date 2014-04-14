620a69c15dbafa8c6e1b893def4d9e11efd8615d
jiangwuzhang <jiangwuzhang@91waijiao.com>
2014-04-14 11:55:47 +0800

    update

---
+++ library/models/credit/credit_model.php
@@ -0,0 +1,48 @@
+<?php
+
+class credit_model extends LI_Model {
+	
+	public function change_add($user_id, $foreign_id, $credit_change, $msg = ""){
+		if ($credit_change <= 0){
+			return -1;			
+		}
+		
+		$this->db->trans_start();
+		$sql_str["row_credit"] = "select id,balance,total from credit where id=?";
+		$credit = $this->db->query($sql_str["row_credit"], array($user_id))->row_array();
+		if (!isset($credit["balance"])){
+			$sql_str["update_credit"] = "insert into credit(id,balance,total) values(?,?,?)";
+			$this->db->query($sql_str["update_credit"], array($user_id, $credit_change, $credit_change));
+		} else {
+			$balance = $credit["balance"] + $credit_change;			//帐变后积分剩余
+			$total = $credit["total"] + $credit_change;
+			$sql_str["update_credit"] = "UPDATE credit SET balance=?,total=? WHERE id=?";
+			$this->db->query($sql_str["update_credit"], array($balance, $total, $user_id));
+		}
+		if ($this->db->affected_rows() === 1){
+			$sql_str["create_credit_log"] = "INSERT INTO credit_logs(user_id,foreign_id,credit_change,msg) VALUES(?,?,?,?)";
+			$this->db->query($sql_str["create_credit_log"], array($user_id, $foreign_id, $credit_change, $msg));
+		} else {
+			$this->db->trans_rollback();
+		}
+		
+		$this->db->trans_complete();
+		if ($this->db->trans_status() === false){
+			return false;
+		}
+		return true;
+	}
+	
+	public function get($user_id){
+		$res = $this->db->query("select balance,total from credit where id=?", array($user_id))->row_array();
+		if (!isset($res["balance"])){
+			$res["balance"] = 0;
+			$res["total"] = 0;
+			$this->db->query("insert into credit(id,balance,total) values(?,?,?)", 
+				array($user_id, $res["balance"], $res["total"]));
+		}
+		return $res;
+	}
+}
+
+/* end of credit_model.php */
