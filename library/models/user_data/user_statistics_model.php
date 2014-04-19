354e4b9b83eb34cce8b320e8fcd8a6f1970c9cad
tangxunye <tangxunye@91waijiao.com>
2014-04-19 16:50:40 +0800

    update

--- library/models/user_data/user_statistics_model.php
+++ library/models/user_data/user_statistics_model.php
@@ -29,22 +29,15 @@ class User_Statistics_Model extends MY_Model {
 
     public function get_teacher_statistics($user_id)
     {
-        if($this->redis_model->connect('download'))
-        {
-            $this->_paper_month_key=date('Y-m').'_paper_'.$user_id;
-            $this->_homework_month_key=date('Y-m').'_homework_'.$user_id;
-
-            $statistics['paper']=$this->cache->get($this->_paper_month_key);
-            $statistics['homework']=$this->cache->get($this->_homework_month_key);
-        }
-        
         $this->load->model("class/classes");
         $class_static = $this->classes->class_static($user_id);
         $this->load->model('cloud/cloud_model');
         $statistics['share_total'] = $this->cloud_model->share_file_total( 0, $user_id);
-        $statistics['cloud_storage'] = $this->cloud_model->get_user_cloud_storage($user_id,true);
-        $this->load->model('exercise_plan/teacher_exercise_plan_model');
-        $statistics['hw_total'] = $this->teacher_exercise_plan_model->teacher_ex_total( $user_id );
+        //debug
+        $statistics['cloud_storage']['use_storage']=$statistics['hw_total']=0;
+        //$statistics['cloud_storage'] = $this->cloud_model->get_user_cloud_storage($user_id,true);
+        //$this->load->model('exercise_plan/teacher_exercise_plan_model');
+        //$statistics['hw_total'] = $this->teacher_exercise_plan_model->teacher_ex_total( $user_id );
         $statistics['file_total']=$this->cloud_model->teacher_file_total( $user_id );
         $statistics = array_merge($statistics, $class_static);
 
@@ -55,16 +48,27 @@ class User_Statistics_Model extends MY_Model {
         $this->smarty->assign('userlevel',$privilege['userlevel']);
         //end userlevel
 
+        //paper
+        if($this->redis_model->connect('download'))
+        {
+            $this->_paper_month_key=date('Y-m').'_paper_'.$user_id;
+            $this->_homework_month_key=date('Y-m').'_homework_'.$user_id;
+            $statistics['paper']=$this->cache->get($this->_paper_month_key);
+            $statistics['homework']=$this->cache->get($this->_homework_month_key);
+        }
         if(!isset($statistics['paper'])||!$statistics['paper']) $statistics['paper']=0;
         if(!isset($statistics['homework'])||!$statistics['homework']) $statistics['homework']=0;
-
         $statistics['paper_limit']=$privilege['privilege']['paper_permonth']['value'];
         $statistics['homework_limit']=$privilege['privilege']['homework_permonth']['value'];
+
         /*备课文档下载统计 begin*/
-        $this->load->model('lesson/document_download_model','doc_down');
-        $statistics['lesson_month_down'] = $this->doc_down->get_lesson_down_statistics($user_id,'m');
+        //debug
+        $statistics['lesson_month_down']=0;
+        //$this->load->model('lesson/document_download_model','doc_down');
+        //$statistics['lesson_month_down'] = $this->doc_down->get_lesson_down_statistics($user_id,'m');
         $statistics['lesson_down_limit'] = $privilege['privilege']['lesson_permonth']['value'];
         /*备课文档下载统计 end*/
+
         return $statistics;
     }
 
