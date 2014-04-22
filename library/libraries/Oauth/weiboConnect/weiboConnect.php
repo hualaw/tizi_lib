293e26f13dde9da1d4d97cbbc06d305ec8cd5758
wangguangmao <wangguangmao@91waijiao.com>
2014-04-22 18:21:44 +0800

    weibo oauth

--- library/libraries/Oauth/weiboConnect/weiboConnect.php
+++ library/libraries/Oauth/weiboConnect/weiboConnect.php
@@ -5,42 +5,48 @@ include_once( dirname(__DIR__).DIRECTORY_SEPARATOR.'Connect.php' );
 
 class weiboConnect extends Connect{
 
-    private $connect;
     private $config;
 
     public function __construct(){
 
         self::$module = 'weibo';
         $this->config = $this->get_config();
-        $this->connect = new SaeTOAuthV2( $this->config['appid'] , $this->config['appkey'] );
 
     }
 
     public function login(){
 
-        $code_url = $this->connect->getAuthorizeURL( WB_CALLBACK_URL );
+        $connect = new SaeTOAuthV2( $this->config['appid'] , $this->config['appkey'] );
+        $code_url = $connect->getAuthorizeURL( $this->config['callback'] );
         header("Location:$code_url");
 
     }
 
     public function callback(){
 
+        $connect = new SaeTOAuthV2( $this->config['appid'] , $this->config['appkey'] );
+        $code_url = $connect->getAuthorizeURL( $this->config['callback'] );
         if (isset($_REQUEST['code'])) {
             $keys = array();
             $keys['code'] = $_REQUEST['code'];
             $keys['redirect_uri'] = $this->config['callback'];
             try {
-                $token = $this->connect->getAccessToken( 'code', $keys ) ;
+                $token_data = $connect->getAccessToken( 'code', $keys ) ;
+                $token_val = $token_data['access_token'];
+                $client_connect = new SaeTClientV2( $this->config['appid'] ,  $this->config['appkey'] , $token_val );
             } catch (OAuthException $e) {
-
+                echo $e->getMessage();
             }
+            $user = $client_connect->get_uid();
+            $open_id = 0;
+            if(isset($user['uid'])){
+                $open_id = $user['uid'];
+            }
+            return  array(
+                'access_token'=>$token_val,
+                'open_id'=>$open_id
+            );
         }
-        $open_id = $this->connect->get_uid();
-
-        return  array(
-            'access_token'=>$access_token,
-            'open_id'=>$open_id
-        );
     }
 
 }
