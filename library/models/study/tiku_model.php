553ef0297ba826df1555bdd99a65c7e3b824e29b
liubing <liubing@tizi.com>
2014-05-06 14:45:30 +0800

    登陆，注册，获取榜单用户列表，个人信息接口，统一一下，返回nick_name字段

--- library/models/study/tiku_model.php
+++ library/models/study/tiku_model.php
@@ -36,7 +36,7 @@ Class Tiku_model extends LI_Model
 	 */
 	public function searchComrade($search)
 	{
-		$result = $this->db->query("select u.id as user_id,u.name,sd.pet_id,sd.subject_type,sd.location_id from user_data sd
+		$result = $this->db->query("select u.id as user_id,u.name as nick_name,sd.pet_id,sd.subject_type,sd.location_id from user_data sd
 				left join user u on u.id=sd.user_id where u.name like '".$search."%'")->result_array();
 		return $result;
 	}
@@ -86,7 +86,7 @@ Class Tiku_model extends LI_Model
 	 */
 	public function weekRanking($user_id,$rows,$offset){
 		$result = $this->db->query("select sd.user_id,sd.pet_id,sd.subject_type,
-				sd.location_id,suws.exp as experience,u.name from user_data sd
+				sd.location_id,suws.exp as experience,u.name as nick_name from user_data sd
 				left join user u on sd.user_id = u.id
 				left join study_user_week_stat suws on sd.user_id = suws.userId
 				where sd.user_id = ".$user_id." order by suws.exp desc
@@ -99,7 +99,7 @@ Class Tiku_model extends LI_Model
 	 */
 	public function totleRanking($user_id,$rows,$offset){
 		$result = $this->db->query("select sd.user_id,sd.pet_id,sd.subject_type,sd.location_id,
-				sd.exp as experience,u.name from user_data sd
+				sd.exp as experience,u.name as nick_name from user_data sd
 				left join user u on sd.user_id = u.id
 				where sd.user_id = ".$user_id." order by sd.exp desc
 				limit ".$rows.','.$offset)->row_array();
@@ -111,7 +111,7 @@ Class Tiku_model extends LI_Model
 	 */
 	public function getTopstudentList($userInfo,$rows,$offset)
 	{
-		$result = $this->db->query("select sd.user_id,u.name,sd.pet_id,sd.exp as experience 
+		$result = $this->db->query("select sd.user_id,u.name as nick_name,sd.pet_id,sd.exp as experience 
 				from user_data sd left join user u on sd.user_id = u.id 
 				order by sd.exp desc limit ".$rows.','.$offset)->result_array();
 		return $result;
@@ -126,7 +126,9 @@ Class Tiku_model extends LI_Model
 
 		foreach ($result as $k=>$v)
 		{
-			$nums = $this->db->query("select id from study_user_relation where userId = ".$userInfo['user_id']." and friendId = ".$v['user_id'])->num_rows();
+			$nums = $this->db->query("
+			        select id from study_user_relation 
+			        where userId = ".$userInfo['user_id']." and friendId = ".$v['user_id'])->num_rows();
 			if ($nums > 0)
 			{
 				$result[$k]['is_follow'] = 1;
