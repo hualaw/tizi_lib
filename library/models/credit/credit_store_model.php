77299f120c292ad0018b8bd00ecf2b73262a1064
jiangwuzhang <jiangwuzhang@91waijiao.com>
2014-07-09 17:35:46 +0800

    store

--- library/models/credit/credit_store_model.php
+++ library/models/credit/credit_store_model.php
@@ -8,6 +8,40 @@ class credit_store_model extends LI_Model {
 		return $res;
 	}
 	
+	public function get($id, $fields = "*"){
+		$res = $this->db->query("select {$fields} from credit_store where id=?", array($id))->row_array();
+		return $res;
+	}
+	
+	public function pay($user_id, $goods_id){
+		$goods = $this->get($goods_id);
+		$this->load->model("credit/credit_model");
+		$balance = $this->credit_model->get($user_id);
+		if ($balance["balance"] >= $goods["credit_price"]){
+			$this->db->trans_start();
+			$this->db->query("UPDATE credit SET balance=balance-? WHERE id=?", array($goods["credit_price"], $user_id));
+			$this->db->query("INSERT INTO credit_orders(user_id,goods_id,status,create_date) VALUES(?,?,?,?)", array(
+				$user_id, $goods_id, Constant::CREDIT_ORDER_STATUS_NEW, date("Y-m-d H:i:s")));
+			$foreign_id = $this->db->insert_id();
+			
+			$msg = "兑换 ".$goods["name"];
+			$this->db->query("INSERT INTO credit_logs(user_id,foreign_id,credit_change,total,msg,cyclenum) VALUES(?,?,?,?,?,?)", array(
+				$user_id, $foreign_id, 0-$goods["credit_price"], $balance["balance"]-$goods["credit_price"], $msg, 0));
+			$this->db->trans_complete();
+			if ($this->db->trans_status() === false){
+				return false;
+			}
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+	
+	public function get_order($order_id, $fields = "*"){
+		$res = $this->db->query("select {$fields} from credit_orders where id=?", array($order_id))->row_array();
+		return $res;
+	}
+	
 }
 
 /* end of credit_store_model.php */
