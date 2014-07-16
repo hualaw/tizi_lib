78bb2c285c43dafef8ee25de33156fb7e819a961
tangxunye <tangxunye@91waijiao.com>
2014-07-16 13:41:18 +0800

    update

--- library/views/static/lib/evercookie/0.4.0/php/evercookie_etag.php
+++ library/views/static/lib/evercookie/0.4.0/php/evercookie_etag.php
@@ -42,7 +42,7 @@ if (!isset($_COOKIE['evercookie_etag']) || empty($_COOKIE['evercookie_etag'])) {
 
     $headers = apache_request_headers();
     if(isset($headers['If-None-Match'])) {
-		echo $headers['If-None-Match'];
+		//echo $headers['If-None-Match'];
 	}
 
     header('Etag: '.$headers["If-None-Match"]);
