4e9394f0120691d330ed7391571896d1cd9df543
tangxunye <tangxunye@91waijiao.com>
2014-07-17 14:31:57 +0800

    update

--- library/views/static/lib/tizi_common/0.0.1/tizi_cookie.js
+++ library/views/static/lib/tizi_common/0.0.1/tizi_cookie.js
@@ -3,65 +3,53 @@ define(function(require, exports) {
 	require('everCookie');
 	require('flashCookie');
 
-	$.ajax({
-		type:"GET",
-		url:"http://lk.brand.sogou.com/svc/getyyid.php",
-		dataType:"jsonp",
-		callback:"idcb",
-		success:function(){},
-		error:function(){}
-	})
+	// $.ajax({
+	// 	type:"GET",
+	// 	url:"http://lk.brand.sogou.com/svc/getyyid.php",
+	// 	dataType:"jsonp",
+	// 	callback:"idcb",
+	// 	success:function(){},
+	// 	error:function(){}
+	// })
 
-	idcb = function(yyid){
+	var ec = new evercookie({
+		baseurl: staticBaseUrlName + staticVersion + 'lib/evercookie/0.4.0',
+		asseturi: '/assets',
+		phpuri: '/php'
+	});
 
-		var ec = new evercookie({
-			baseurl: staticBaseUrlName + staticVersion + 'lib/evercookie/0.4.0',
-			asseturi: '/assets',
-			phpuri: '/php'
-		});
+	var fcuid = null;
+	var debug = true;
 
-		var fcuid = null;
-
-		var fc = new SwfStore({
-			swf_url: staticBaseUrlName + staticVersion + 'lib/flashcookie/1.9.1/storage.swf',
-			onready: function(){
-				fcuid = fc.get("uid");
-			},
-			onerror: function(){
+	var fc = new SwfStore({
+		swf_url: staticBaseUrlName + staticVersion + 'lib/flashcookie/1.9.1/storage.swf',
+		onready: function(){
+			fcuid = fc.get("uid");
+		},
+		onerror: function(){
+			if(debug) {
 				document.cookie = "cookie_debug=errorfc";
+			}
+		}
+	});
 
+	ec.get("uid", function(ecuid, all) {
+		if(!fcuid) {
+			if(typeof fc == 'object') {
+				fcuid = ecuid;
+				fc.set("uid", fcuid);
+				msg = "fcset:"+fcuid;
 			}
-		});
-
-		ec.get("uid", function(ecuid, all) {
-			if(yyid) {
-				if(ecuid != yyid) {
-					ecuid = yyid
-					ec.set("uid", ecuid);
-					msg = "ecyset:"+ecuid;
-				}
-				if(fcuid != yyid) {
-					fcuid = yyid;
-					fc.set("uid", fcuid);
-					msg = "fcyset:"+fcuid;
-				}
-			}
-			if(!fcuid) {
-				if(typeof fc == 'object') {
-					fcuid = ecuid;
-					fc.set("uid", fcuid);
-					msg = "fcset:"+fcuid;
-				}
-			} else if(fcuid != ecuid) {
-				ecuid = fcuid;
-				ec.set("uid", ecuid);
-				msg = "ecset:"+fcuid;
-			} else {
-				msg = "uid:"+ecuid;
-			}
+		} else if(fcuid != ecuid) {
+			ecuid = fcuid;
+			ec.set("uid", ecuid);
+			msg = "ecset:"+fcuid;
+		} else {
+			msg = "uid:"+ecuid;
+		}
+		if(debug) {
 			document.cookie = "cookie_debug="+msg;
-		}, 0);
-	}
-
+		}
+	}, 0);
 	
 });
