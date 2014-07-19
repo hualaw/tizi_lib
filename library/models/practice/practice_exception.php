250e0d4c2cb2bcce8409a7b0d2d709325ccd0917
saeed <wangguangmao@tizi.com>
2014-07-19 15:29:41 +0800

    add practice model

---
+++ library/models/practice/practice_exception.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author saeed
+ * @date   2013-10-17
+ * @description 专项练习 - 异常处理 
+ */
+class Practice_Exception extends Exception{
+    
+    public function __construct(){
+        parent::__construct();
+    }
+
+}
