1bbc7548b4b0b2c1e5537974567caffc102d40b2
linzhen <linzhen@91waijiao.com>
2014-07-26 15:04:38 +0800

    add teacher id

--- library/models/homework/student_zuoye_model.php
+++ library/models/homework/student_zuoye_model.php
@@ -10,7 +10,7 @@ class Student_Zuoye_Model extends MY_Model {
 
     public function get($where){
 
-        $this->db->select('zuoye_student.*, zuoye_assign.video_ids, zuoye_assign.unit_game_ids,zuoye_assign.start_time,zuoye_assign.end_time, zuoye_comment.content');
+        $this->db->select('zuoye_student.*, zuoye_assign.video_ids, zuoye_assign.user_id as teacher_id, zuoye_assign.unit_game_ids,zuoye_assign.start_time,zuoye_assign.end_time, zuoye_comment.content');
         $this->db->from('zuoye_student');
         $this->db->join('zuoye_assign', 'zuoye_assign.id = zuoye_student.zy_assign_id', 'left');
         $this->db->join('zuoye_comment', 'zuoye_student.zy_assign_id = zuoye_comment.zy_assign_id and zuoye_student.user_id = zuoye_comment.user_id', 'left');
