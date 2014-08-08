3cd4b9cf2c2626353a7fb2f4e1d160c45c0872c7
tangxunye <tangxunye@91waijiao.com>
2014-08-08 19:42:53 +0800

    udpate

--- library/core/LI_Controller.php
+++ library/core/LI_Controller.php
@@ -306,10 +306,12 @@ class LI_Controller extends CI_Controller{
 						$login_redirect=$this->input->get_post('redirect',true,false,'reload');
 						$reg_redirect=$this->input->get_post('reg_redirect',true);
 						$reg_role=$this->input->get_post('reg_role',true,true,$this->reg_role);
+						$reg_url=$this->input->get_post('reg_url',true,true,'');
 						$this->smarty->assign('login_url',login_url());
 						$this->smarty->assign('login_redirect',$login_redirect);
 						$this->smarty->assign('reg_redirect',$reg_redirect);
 						$this->smarty->assign('reg_role',$reg_role);
+						$this->smarty->assign('reg_url',$reg_url);
 						$html=$this->smarty->fetch('[lib]common/tizi_login_form.html');
 				    	echo json_ntoken(array('errorcode'=>false,'error'=>$this->lang->line('default_error_login'),'login'=>false,'html'=>$html,'redirect'=>$login_redirect,'token'=>false,'code'=>1));
 					    exit();
