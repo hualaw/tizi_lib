0d72bf8eb0b0864f2a07be4d15df65e691673fa1
linzhen <linzhen@91waijiao.com>
2014-08-13 11:21:39 +0800

    zuoye func

--- library/models/homework/stu_zuoye_model.php
+++ library/models/homework/stu_zuoye_model.php
@@ -132,14 +132,19 @@ class Stu_Zuoye_Model extends LI_Model{
                 if(isset($val['paper_ids']) and $val['paper_ids']){
                     $this->load->model('exercise_plan/student_homework_model');
                     $paper_ids = json_decode($val['paper_ids'],true);
+                    $paper_complete_sum = 0;
                     foreach($paper_ids as $pas=>$p){//$val['user_id'] == student_id
-                        $papers[] = $this->student_homework_model->get_student_homework($val['user_id'],$p['assignment_id']);
+                        $_pap = $this->student_homework_model->get_student_homework($val['user_id'],$p['assignment_id']);
+                        if($_pap->is_completed){
+                            $paper_complete_sum ++;
+                        }
+                        $papers [] = $_pap;
                         // var_dump($papers);
                     }
                 }
             }
-            $val['task_num'] = count($videos) + count($games);
-            $val['task_num_completed'] = count($student_video) + count($student_game);
+            $val['task_num'] = count($videos) + count($games) + count($papers);
+            $val['task_num_completed'] = count($student_video) + count($student_game) + $paper_complete_sum;
             $val['videos'] = $videos;
             $val['games'] = $games;
             $val['papers'] = $papers;
