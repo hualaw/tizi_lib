37b4cb2a02abe86c54e2dbcda1984c7e464c3b59
windguo <webmaster@windguo.com>
2014-08-16 15:54:39 +0800

    修改首页登陆样式以及相关逻辑

--- library/views/static/lib/tizi_common/0.0.1/tizi_login_school.js
+++ library/views/static/lib/tizi_common/0.0.1/tizi_login_school.js
@@ -5,52 +5,66 @@ define(function(require,exports){
 	};
 	// 选择省份
 	exports.selectProvince = function(){
-		$('#cmbProvince').change(function(){
-			// 调用默认值
-			$('#cmbCity').html('<option value="">请选择市</option>');
-			$('#schoolNameSelect').html('<option value="">请选择学校</option>');
-			// 获取当前选择的省份
-			var _proValue = $(this).val();
-			$.ajax({
-				url:baseUrlName + 'class/agents_school/get_city?province_id=' + _proValue,
-				'type' : 'GET',
-				'dataType' : 'json',
-				success : function(json, status){
-					// 循环json数据结果
-					var listr = '<option value="">请选择市</option>';
-					for (var i = 0; i < json.length; i++){
-	                    listr += '<option value="'+json[i].city_id+'">'+json[i].city_name+'</option>';
-	                };
-	                // 插入到select
-	                $('#cmbCity').html(listr);
-	                // 调用选择市
-	                exports.selectCity();
-				}
+		$('#cmbProvince li').each(function(){
+			$(this).click(function(){
+				// 恢复学校为默认值
+				$('#cmbCity ul').html('<li class="grey">请稍候...</li>');
+				$('#schoolNameSelect ul').html('<li></li>');
+				// 为当前li添加active
+				$(this).addClass('active').siblings().removeClass('active');
+				// 获取当前选择的省份
+				var _proValue = $(this).val();
+				$.ajax({
+					url:baseUrlName + 'class/agents_school/get_city?province_id=' + _proValue,
+					'type' : 'GET',
+					'dataType' : 'json',
+					success : function(json, status){
+						// 循环json数据结果
+						var listr = '';
+						for (var i = 0; i < json.length; i++){
+		                    listr += '<li value="'+json[i].city_id+'">'+json[i].city_name+'</li>';
+		                };
+		                // 插入到select
+		                $('#cmbCity ul').html(listr);
+		                exports.selectCity();
+					}
+				});
 			})
 		});
-	}
+	};
 	// 选择市
 	exports.selectCity = function(){
-		$('#cmbCity').change(function(){
-			// 默认值
-			$('#schoolNameSelect').html('<option value="">请选择学校</option>');
-			// 获取当前选择的城市
-			var _cityValue = $(this).val();
-			$.ajax({
-				url:baseUrlName + 'class/agents_school/get_school?city_id=' + _cityValue,
-				'type' : 'GET',
-				'dataType' : 'json',
-				success : function(json, status){
-					// 循环json数据结果
-					var listr = '<option value="">请选择学校</option>';
-					for (var i = 0; i < json.length; i++){
-	                    listr += '<option value="'+json[i].school_id+'">'+json[i].schoolname+'</option>';
-	                };
-	                console.log(listr);
-	                // 插入到select
-	                $('#schoolNameSelect').html(listr);
-				}
+		$('#cmbCity li').each(function(){
+			$(this).click(function(){
+				$('#schoolNameSelect ul').html('<li class="grey">请稍候...</li>');
+				$(this).addClass('active').siblings().removeClass('active');
+				// 获取当前选择的城市
+				var _cityValue = $(this).val();
+				$.ajax({
+					url:baseUrlName + 'class/agents_school/get_school?city_id=' + _cityValue,
+					'type' : 'GET',
+					'dataType' : 'json',
+					success : function(json, status){
+						// 循环json数据结果
+						var listr = '';
+						for (var i = 0; i < json.length; i++){
+		                    listr += '<li value="'+json[i].school_id+'">'+json[i].schoolname+'</li>';
+		                };
+		                // console.log(listr);
+		                // 插入到select
+		                $('#schoolNameSelect ul').html(listr);
+		                exports.selectSchool();
+					}
+				});
 			})
-		})
+		});
+	};
+	// 选择学校
+	exports.selectSchool = function(){
+		$('#schoolNameSelect li').each(function(){
+			$(this).click(function(){
+				$(this).addClass('active').siblings().removeClass('active');
+			})
+		});
 	}
 });
