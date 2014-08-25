2085b923d7fe306fbf0c6a272dad7c2a5b926e1c
tangxunye <tangxunye@91waijiao.com>
2014-08-25 13:38:20 +0800

    upadte

--- library/database/DB.php
+++ library/database/DB.php
@@ -165,21 +165,21 @@ function &DB($params = '', $active_record_override = NULL)
 		$active_record = $active_record_override;
 	}
 
-	if(!file_exists($file_path = LIBPATH.'database/DB_driver.php'))
-	{
-		$file_path = BASEPATH.'database/DB_driver.php';
-	}
-	require_once($file_path);
-	//require_once(BASEPATH.'database/DB_driver.php');
+	// if(!file_exists($file_path = LIBPATH.'database/DB_driver.php'))
+	// {
+	// 	$file_path = BASEPATH.'database/DB_driver.php';
+	// }
+	// require_once($file_path);
+	require_once(BASEPATH.'database/DB_driver.php');
 
 	if ( ! isset($active_record) OR $active_record == TRUE)
 	{
-		if(!file_exists($file_path = LIBPATH.'database/DB_active_rec.php'))
-		{
-			$file_path = BASEPATH.'database/DB_active_rec.php';
-		}
-		require_once($file_path);
-		//require_once(BASEPATH.'database/DB_active_rec.php');
+		// if(!file_exists($file_path = LIBPATH.'database/DB_active_rec.php'))
+		// {
+		// 	$file_path = BASEPATH.'database/DB_active_rec.php';
+		// }
+		// require_once($file_path);
+		require_once(BASEPATH.'database/DB_active_rec.php');
 
 		if ( ! class_exists('CI_DB'))
 		{
@@ -194,12 +194,12 @@ function &DB($params = '', $active_record_override = NULL)
 		}
 	}
 
-	if(!file_exists($file_path = LIBPATH.'database/drivers/'.$params['dbdriver'].'/'.$params['dbdriver'].'_driver.php'))
-	{
-		$file_path = BASEPATH.'database/drivers/'.$params['dbdriver'].'/'.$params['dbdriver'].'_driver.php';
-	}
-	require_once($file_path);
-	//require_once(BASEPATH.'database/drivers/'.$params['dbdriver'].'/'.$params['dbdriver'].'_driver.php');
+	// if(!file_exists($file_path = LIBPATH.'database/drivers/'.$params['dbdriver'].'/'.$params['dbdriver'].'_driver.php'))
+	// {
+	// 	$file_path = BASEPATH.'database/drivers/'.$params['dbdriver'].'/'.$params['dbdriver'].'_driver.php';
+	// }
+	// require_once($file_path);
+	require_once(BASEPATH.'database/drivers/'.$params['dbdriver'].'/'.$params['dbdriver'].'_driver.php');
 	
 	// Instantiate the DB adapter
 	$driver = 'CI_DB_'.$params['dbdriver'].'_driver';
