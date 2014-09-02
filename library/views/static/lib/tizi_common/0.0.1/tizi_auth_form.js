c39c7db6cb587a13ef4d677a2014004dd2aacbd3
windguo <webmaster@windguo.com>
2014-09-02 18:02:51 +0800

    下载试卷的手机验证

--- library/views/static/lib/tizi_common/0.0.1/tizi_auth_form.js
+++ library/views/static/lib/tizi_common/0.0.1/tizi_auth_form.js
@@ -1,20 +1,72 @@
 define(function(require, exports) {
     require('tiziDialog');
     require('tizi_ajax');
-    
+    // 请求验证库
+    require("validForm");
+    // 请求公共验证信息
+    var sDataType = require("tizi_datatype").dataType();
+
     exports.authForm = function(html){
         $.tiziDialog({
             id:'authFormID',
         	title:'手机验证',
             content:html,
             icon:null,
-            width:400,
-            ok:false,
+            width:530,
+            ok:function(){
+                $('.modifyMyPhoneFormSubmit').submit();
+                return false;
+            },
             close:function(){
                 
             }
         });
-        //require("tizi_valid").indexAuth();
+        // 发送手机验证码
+        seajs.use('tizi_msgsend',function(ex){
+          ex.sms.init($('.modifyPhone').val(),4,'.modifyPhone');
+        });
+        // 验证规则
+        var _Form = $(".modifyMyPhoneForm").Validform({
+            // 自定义tips在输入框上面显示
+            tiptype: 3,
+            showAllError: false,
+            beforeSubmit: function(curform) { 
+                // 加载MD5加密
+                require("tizi_validform").md5(curform);
+            },
+            ajaxPost: true,
+            callback: function(data) {
+                if(data.errorcode){
+                    // var phone = $('.modifyPhone').val();
+                    // phone = phone.substr(0,3) + '****' + phone.substr(7,10);
+                    // var html = '<span class="bindPhoneInfo"><em class="green">' + phone + '</em></span>';
+                    // $('.bindPhoneInfo').html(html);
+                    // $('#bindPhoneStatus').addClass('current');
+                    // $.tiziDialog({
+                    //     icon: 'succeed',
+                    //     content:data.error,
+                    //     close:function(){
+                    //         $('.modifyMyPhoneForm ul').slideUp();
+                    //     }
+                    // });
+                }else{
+                    $.tiziDialog({icon:"error",content:data.error});
+                }
+            }
+        });
+        _Form.addRule([{
+                ele: ".modifyPhone",
+                datatype: sDataType.Phone.datatype,
+                nullmsg: sDataType.Phone.nullmsg,
+                errormsg: sDataType.Phone.errormsg
+            },{
+                ele: ".forgetTelCaptap",
+                datatype: sDataType.PhoneCode.datatype,
+                nullmsg: sDataType.PhoneCode.nullmsg,
+                errormsg: sDataType.PhoneCode.errormsg
+            }
+            
+        ]);
     }
 
     exports.authCheckClick = function(){
