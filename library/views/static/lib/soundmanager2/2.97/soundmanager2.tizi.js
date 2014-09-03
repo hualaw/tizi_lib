685b4ff4f1b5bfa9685c058ac092a57a988eaf82
tangxunye <tangxunye@91waijiao.com>
2014-09-03 15:04:56 +0800

    update

--- library/views/static/lib/soundmanager2/2.97/soundmanager2.tizi.js
+++ library/views/static/lib/soundmanager2/2.97/soundmanager2.tizi.js
@@ -3390,7 +3390,7 @@ function BasicMP3Player() {
       sm = soundManager, // soundManager instance
       isTouchDevice = (navigator.userAgent.match(/ipad|iphone/i)),
       isIE = (navigator.userAgent.match(/msie/i));
-  this.excludeClass = 'button-exclude'; // CSS class for ignoring MP3 links
+  this.excludeClass = 'sm2_exclude'; // CSS class for ignoring MP3 links
   this.links = [];
   this.sounds = [];
   this.soundsByURL = {};
@@ -3536,8 +3536,10 @@ function BasicMP3Player() {
     }
     sURL = o.getAttribute('url');
     if (!sURL || !soundManager.canPlayLink(o) || self.classContains(o,self.excludeClass)) {
-      sm._writeDebug('pass-thru for non-MP3/non-links');
-      self.addClass(o,self.css.sNoplay);
+      if (self.classContains(o,self.css.sDefault) && !self.classContains(o,self.excludeClass)) {
+        sm._writeDebug('pass-thru for non-MP3/non-links');
+        self.addClass(o,self.css.sNoplay);
+      }
       return true; // pass-thru for non-MP3/non-links
     }
     if (!self.classContains(o,self.includeClass)) {
